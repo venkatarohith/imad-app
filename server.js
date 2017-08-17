c88f6d3f93aedb00413cb34d1bcca89f9575f76f
venkatarohith <30724926+venkatarohith@users.noreply.github.com>
2017-08-17 13:09:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,34 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/rohith', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'rohith.html'));
+});
+
+app.get('/ui/page1', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'page1.html'));
+});
+
+app.get('/ui/page2', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'page2.html'));
+});
+
+app.get('/ui/page3', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'page3.html'));
+});
+
+app.get('/ui/page4', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'page4.html'));
+});
+
+app.get('/ui/contact', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'contact.html'));
+});
+
+app.get('/ui/styles.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'styles.css'));
+});
+
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
